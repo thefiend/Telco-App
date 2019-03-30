b23fd9751a6000a56df631a034102ee407947835
Ng Han Inn <>
2019-03-30 18:10:32 +0800

    update 2

--- nodeJS_Server/testing/testing.js
+++ nodeJS_Server/testing/testing.js
@@ -1,6 +1,11 @@
+//{"number":"91234567", "addon": { "id":"voice_100", "name":"100 minutes", "price":"5", "value":"6000"}}
+
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json()); // for parsing application/json
 
 var admin = require("firebase-admin");
 
@@ -19,6 +24,18 @@ app.get('/dashboard/:number', (req, res) => {
 	ref.once("value", function(snapshot) {
 		res.json(snapshot.val());
 	})
+})
+
+app.get('/addon/:type', (req, res) => {
+	var ref = db.ref("addons/"+req.params.type);
+	ref.once("value", function(snapshot) {
+		res.json(snapshot.val());
+	})
+})
+
+app.post('/addon/subscribe/', (req, res) => {
+	//var ref = db.ref
+	res.json(req.body)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
