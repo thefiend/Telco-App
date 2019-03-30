47cc2666aa49288db29db342a47a6420c5e13b26
jylee-git <jylee.w94@gmail.com>
2019-03-30 16:57:16 +0800

    Update myFirst.js

--- nodeJS_Server/myFirst.js
+++ nodeJS_Server/myFirst.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/dashboard/:number', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Number entered: ' + req.params.number)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
